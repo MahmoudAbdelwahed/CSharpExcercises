8efd4413b9b6016ec3b3b2a0ab1a50fb7fe9270d
MahmoudAbdelwahed <m-abdelwahed@hotmail.com>
2020-12-07 15:42:14 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            //--------------------------------------------------------------------------
+            //// 10th excercise calculate and print the average
+            ///Enter first number - 5
+            //            Enter second number - 6
+            //Enter third number - 7
+            //
+
+
+            //Expected Output:
+            //Result of specified numbers 5, 6 and 7, (x + y).z is 77 and x.y + y.z is 72
+
+            //Console.WriteLine("First number");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Second number");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("third number");
+            //double num3 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Result of specified numbers {0},{1},{2},({3}+{4}).{5} is {6} and {7}.{8}+{9}.{10} is {11} ", num1,num2,num3,num1,num2,num3,(num1+num2)*num3,num1,num2,num2,num3,((num1*num2)+(num2*num3)));
+            ////Console.WriteLine("Result of specified numbers {0},{1},{2},({3}+{4}).{5} is {6} and {7}.{8}+{9}.{10} is {11}",num1,num2,num3,num1,num2,num3,(num1+num2)*num3))
+
+
+
+            //--------------------------------------------------------------------------
+            //// 9th excercise calculate and print the average
+            ///
+            // key shortcut  to expand the selection to change ToDouble method in multiline click contorl +option and set the cursor in the 4 positions change it once
+            //Console.WriteLine("First number");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Second number");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("third number");
+            //double num3 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("fourth number");
+            //double num4 = Convert.ToDouble(Console.ReadLine());
+
+            //double average = (num1 + num2 + num3 + num4) / 4;
+
+            //Console.WriteLine("The average is " + average);
+
+
+
+            //--------------------------------------------
+            //// 8th excercise multiplication table
+            //
+
+            //Console.WriteLine("First number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i <=10; i++)
+            //{
+            //    Console.WriteLine("{0} *{1} = {2}",num1,i ,num1 * i);
+            //}
+
+
+            //--------------------------------------------------------------------------
+            //// 7th excercise print on screen the output of adding, subtracting, multiplying and dividing of two numbers which will be entered by the user
+            //
+
+            //Console.WriteLine("First number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Second number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            ////Console.WriteLine("third number");
+            ////int num3 = Convert.ToInt32(Console.ReadLine());
+
+            //////int sum = num1 + num2;
+            ////int mult = num1 * num2;
+            ////int Sub = num1 - num2;
+            ////double div = num1 / num2;
+            ////double Mod = num1 % num2;
+
+            //Console.WriteLine("summition of      {0} +  {1} is {2}", num1, num2, num1 + num2);
+            //Console.WriteLine("multiplication of {0} *  {1} is {2}", num1, num2, num1 * num2);
+            //Console.WriteLine("subtraction of    {0} -  {1} is {2}", num1, num2, num1 - num2);
+            //Console.WriteLine("division of       {0} /  {1} is {2}", num1, num2, num1 / num2);
+            //Console.WriteLine("module  of        {0} %  {1} is {2}", num1, num2, num1 % num2);
+
+
+
+            /// //--------------------------------------------------------------------------
+            //// 6th excercise multiply 3 numbers
+            ///
+
+            //Console.WriteLine("First number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Second number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("third number");
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+
+            //int result = num1 * num2 * num3;
+
+            //Console.WriteLine("output:{0}*{1}*{2}={3}", num1, num2, num3, result);
+
+            //--------------------------------------------------------------------------
+            //// 5th excercise swap two numbers
+            ///
+
+            //Console.WriteLine("enter first number");
+            //int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine("enter Second number");
+            //int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //int tempFirst = firstnumber;
+
+            //firstnumber = SecondNumber;
+            //SecondNumber = tempFirst;
+
+            //Console.WriteLine("After Swapping, First Number becomes {0} and second number becomes: {1}", firstnumber, SecondNumber);
+
+
+            //--------------------------------------------------------------------------
+
+            //// 4th excercise arithmatic operattrs
+            ///https://blog.mattclemente.com/2019/07/12/modulus-operator-modulo-operation.html for modulus operator % the remainder
+            //// read from the caller print the result of the specified operations
+            ///
+
+            //            -1 + 4 * 6
+            //(35 + 5) % 7
+            //14 + -4 * 6 / 11
+            //2 + 15 / 6 * 1 - 7 % 2
+
+            //Console.WriteLine("First number");
+            //int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine("Second number");
+            //int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Second number");
+            //int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(-1 + 4 * 6);
+            //Console.WriteLine((35 + 5) % 7);
+            //Console.WriteLine(14+ -4 *6/11);
+
+
+            //--------------------------------------------------------------------------
+
+            ////Second excercise
+            //// read from the caller and divide numbers
+
+            //Console.WriteLine("First number");
+            //int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine("Second number");
+            //int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //int dividetotal = firstNumber / secondNumber;
+
+            //Console.WriteLine("Divide Result is :" + dividetotal);
+
+
+            //--------------------------------------------------------------------------
+
+            // First excercise 
+            // read from the caller and print summition
+
+            //Console.Write("Enter first number:");
+            //int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.Write("Enter Second number:");
+            //int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //int total = firstNumber + SecondNumber;
+
+            //Console.WriteLine("Summtion is:" + total);
+
+            //---------------------------------------------------------------------------
+        }
+    }
+}
